bc7837007b04d17dca68e452d3fd444735e29262
mehmetaksak <63792736+mehmetaksak@users.noreply.github.com>
2024-06-15 19:30:05 +0300

    small changes

---
+++ TaxCalculator.Application/Services/ITaxCalculationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxCalculator.Domain.Entities;
+using TaxCalculator.Domain.ValueObjects;
+
+namespace TaxCalculator.Application.Services
+{
+    public interface ITaxCalculationService
+    {
+        Task<Taxes> CalculateTaxes(TaxPayer taxPayer);
+    }
+}
